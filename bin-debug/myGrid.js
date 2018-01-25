0f933a5694bce42049c4d3e0198adab12cf706ed
tom6262@163.com <tt16880880>
2018-01-25 14:12:40 +0800

    learn log

---
+++ bin-debug/myGrid.js
@@ -0,0 +1,35 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = this && this.__extends || function __extends(t, e) { 
+ function r() { 
+ this.constructor = t;
+}
+for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i]);
+r.prototype = e.prototype, t.prototype = new r();
+};
+var myGrid = (function (_super) {
+    __extends(myGrid, _super);
+    function myGrid() {
+        var _this = _super.call(this) || this;
+        _this.drawGrid();
+        return _this;
+    }
+    myGrid.prototype.drawGrid = function () {
+        this.graphics.beginFill(0x0000ff);
+        this.graphics.drawRect(0, 0, 50, 50);
+        this.graphics.endFill();
+        this.graphics.beginFill(0x0000ff);
+        this.graphics.drawRect(50, 50, 50, 50);
+        this.graphics.endFill();
+        this.graphics.beginFill(0xff0000);
+        this.graphics.drawRect(50, 0, 50, 50);
+        this.graphics.endFill();
+        this.graphics.beginFill(0xff0000);
+        this.graphics.drawRect(0, 50, 50, 50);
+        this.graphics.endFill();
+    };
+    return myGrid;
+}(egret.Shape));
+__reflect(myGrid.prototype, "myGrid");
+//# sourceMappingURL=myGrid.js.map
